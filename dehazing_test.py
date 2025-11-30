7da9d039e13ae1e980764b820e1ad137a8a9f686
Longcodedao <long.vo210404@vnuk.edu.vn>
2025-11-30 23:35:34 +0700

    Clean the ODESolver
    The bug has been solved (No problem at all). The class is returned back
    to the model.flow_matching file

--- dehazing_test.py
+++ dehazing_test.py
@@ -60,43 +60,6 @@ set_seed(42)
 resize_size = 256
 
 
-class ODESolver:
-    def __init__(self, model, nfe=20):
-        self.model = model
-        self.nfe = nfe
-
-    def ode_func(self, t, x):
-        # 1. Ensure the time 't' is a vector of size (B,)
-        # The ODE solver passes 't' as a scaler (if batching is not done internally)
-        # We must expand/broadcast it to match the batch size 'x'
-        t = t.expand(x.size(0))
-        print("t shape is: ", t.shape)
-        print("x shape is: ", x.shape)
-        # 2. Call the UNet (self.model)
-        # The UNet predicts the velocity field (v_theta) given the time and the
-        # image state
-        v_theta = self.model(x, t)
-
-        return v_theta
-
-    @torch.no_grad()
-    def sample(self, x_init):
-        # 1. Define the time span for integration (from 0 to 1, in nfe steps)
-        t_span = torch.linspace(0, 1, self.nfe, device=x_init.device)
-
-        # 2. Define the ODE function for the solver to use
-        # The solver requires a function (t, x) -> dx/dt
-        # We can use the method we just defined:
-        ode_func = self.ode_func
-
-        # 3. Perform the ODE integration
-        solution = odeint(
-            ode_func, x_init, t_span, rtol=1e-5, atol=1e-5, method="euler"
-        )
-        # 4. The solution is a tensor of shape (NFE, B, C, H, W). We return the last state (t=1)
-        return solution[-1]
-
-
 # %%
 ## Trainer
 class DehazeTrainer:
@@ -373,9 +336,6 @@ class DehazeTrainer:
 
             clean_imgs = x1
             hazy_imgs = x0
-
-            print("Clean imgs shape: ", clean_imgs.shape)
-            print("Hazy imgs shape: ", hazy_imgs.shape)
 
             with torch.autocast(device_type=self.device.type):
                 pred_imgs = self.ode_solver.sample(hazy_imgs)
